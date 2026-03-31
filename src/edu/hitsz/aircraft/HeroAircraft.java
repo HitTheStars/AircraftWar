07e50262019f5b40901fa70a3b8d47796436f8e4
HitTheStars <1272063565@qq.com>
2026-03-31 15:45:37 +0800

    Changes

--- src/edu/hitsz/aircraft/HeroAircraft.java
+++ src/edu/hitsz/aircraft/HeroAircraft.java
@@ -10,8 +10,9 @@ import java.util.List;
  * 英雄飞机，游戏玩家操控
  * @author hitsz
  */
-public class HeroAircraft extends AbstractAircraft {
 
+public class HeroAircraft extends AbstractAircraft {
+    private volatile static HeroAircraft heroAircraft;
     //每次射击发射子弹数量
     private int shootNum = 1;
 
@@ -24,6 +25,22 @@ public class HeroAircraft extends AbstractAircraft {
     public HeroAircraft(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
     }
+    public static HeroAircraft getHeroAircraft() {
+        if (heroAircraft == null) {
+            synchronized (HeroAircraft.class) {
+                if (heroAircraft == null) {
+                    heroAircraft = new HeroAircraft();
+                }
+            }
+        }
+        return heroAircraft;
+    }
+}
+public class HeroAircraft extends AbstractAircraft {
+
+
+
+
 
     @Override
     public void forward() {
